3844afdd98fe62d39693e4b6e53f5efc3d6342f6
yhori991 <43234011+yhori991@users.noreply.github.com>
2021-09-20 19:07:57 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from setuptools import setup
-from chembl_structure_pipeline.__version__ import __version__
 
 setup(name='chembl_structure_pipeline',
-      version=__version__,
       description='ChEMBL Structure Pipeline',
       url='https://www.ebi.ac.uk/chembl/',
       author='Greg Landrum',
